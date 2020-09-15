6fe8544255f7e934cad70b54369716ef1ee66c0a
EloyMaddamin <1045031@mborijnland.nl>
2020-09-15 17:15:50 +0200

    Style veranderd van index

--- resources/views/wishData/create.blade.php
+++ resources/views/wishData/create.blade.php
@@ -1,66 +1,66 @@
 @extends('wishData.layout')
 
 @section('content')
-    <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-left">
-                <h2>Add New Wish</h2>
-            </div>
-            <div class="pull-right">
+    <div class="container">
+        <div class="row my-5">
+            <div class="col-4">
                 <a class="btn btn-primary" href="{{ route('wishData.index') }}"> Back</a>
             </div>
+            <div class="col-8">
+                <h2>Add New Wish</h2>
+            </div>
         </div>
+
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        <form action="{{ route('wishData.store') }}" method="POST">
+            {{ csrf_field() }}
+
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Image URL:</strong>
+                        <input type="text" name="img" class="form-control" placeholder="Image URL">
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Name:</strong>
+                        <input type="text" name="name" class="form-control" placeholder="Name">
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Description:</strong>
+                        <input type="text" name="description" class="form-control" placeholder="Description">
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Price:</strong>
+                        <input type="number" class="form-control" name="price" placeholder="Price">
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Product link:</strong>
+                        <input type="text" class="form-control" name="link" placeholder="Product link">
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </div>
+
+        </form>
     </div>
-
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <strong>Whoops!</strong> There were some problems with your input.<br><br>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-
-    <form action="{{ route('wishData.store') }}" method="POST">
-        {{ csrf_field() }}
-
-        <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Image URL:</strong>
-                    <input type="text" name="img" class="form-control" placeholder="Image URL">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Name:</strong>
-                    <input type="text" name="name" class="form-control" placeholder="Name">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Description:</strong>
-                    <input type="text" name="description" class="form-control" placeholder="Description">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Price:</strong>
-                    <input type="number" class="form-control" name="price" placeholder="Price">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Product link:</strong>
-                    <input type="text" class="form-control" name="link" placeholder="Product link">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </div>
-        </div>
-
-    </form>
 @endsection
--- resources/views/wishData/index.blade.php
+++ resources/views/wishData/index.blade.php
@@ -1,17 +1,15 @@
 @extends('wishData.layout')
 
 @section('content')
-    <div class="container-fluid">
-        <div class="my-5 row text-center">
-            <div class="col-4">
-                <a class="btn btn-primary" href="/"> Back</a>
-            </div>
-            <div class="col-4">
-                <h2>Xmas Wishlist</h2>
-            </div>
-            <div class="col-4">
-                <a class="btn btn-success" href="{{ route('wishData.create') }}"> Create New Wish</a>
-            </div>
+    <div class="my-5 row text-center">
+        <div class="col-4">
+            <a class="btn btn-primary" href="/"> Back</a>
+        </div>
+        <div class="col-4">
+            <h2>Xmas Wishlist</h2>
+        </div>
+        <div class="col-4">
+            <a class="btn btn-success" href="{{ route('wishData.create') }}"> Create New Wish</a>
         </div>
     </div>
 
@@ -21,25 +19,25 @@
         </div>
     @endif
 
-    <table class="table table-bordered">
-        <tr>
-            <th>Image</th>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Price</th>
-            <th width="280px">Action</th>
-        </tr>
+
+    <div class="row justify-content-center w-75 m-auto">
         @foreach ($wishData as $wData)
-            <tr>
-                <td><img class="w-75" src="{{ $wData->img }}"></td>
-                <td>{{ $wData->name }}</td>
-                <td>{{ $wData->description }}</td>
-                <td><a target="_blank" href="{{ $wData->link }}">{{ $wData->price }}<br>Buy this</a></td>
-                <td>
+            <div class="col-2">
+                <img class="w-100" src="{{ $wData->img }}">
+            </div>
+            <div class="col-6">
+                <h5>{{ $wData->name }}</h5>
+                <p>{{ $wData->description }}</p>
+            </div>
+            <div class="col-2">
+                <div class="ippa">
+                    <a class="text-decoration-none font-weight-bold" target="_blank"
+                       href="{{ $wData->link }}"><h5>€ {{ $wData->price }}<br>Buy this</h5></a>
+                </div>
+                <div class="mt-5">
                     <form action="{{ route('wishData.destroy',$wData->id) }}" method="POST">
 
                         <a class="btn btn-info" href="{{ route('wishData.show',$wData->id) }}">Show</a>
-
 
 
                         <a class="btn btn-primary" href="{{ route('wishData.edit',$wData->id) }}">Edit</a>
@@ -53,10 +51,13 @@
 
                         <button type="submit" class="btn btn-danger">Delete</button>
                     </form>
-                </td>
-            </tr>
+                </div>
+            </div>
+            <div class="col-12 my-4">
+                <hr class="w-75">
+            </div>
         @endforeach
-    </table>
+    </div>
 
     {!! $wishData->links() !!}
 
