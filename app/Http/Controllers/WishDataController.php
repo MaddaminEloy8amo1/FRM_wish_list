4ca0d4aa033298b84c079fcab00ccd14f69f7af8
EloyMaddamin <1045031@mborijnland.nl>
2020-10-05 15:04:38 +0200

    admin beveiligen

--- app/Http/Controllers/WishDataController.php
+++ app/Http/Controllers/WishDataController.php
@@ -10,6 +10,10 @@ use Illuminate\Http\Request;
 class WishDataController extends Controller
 {
 
+    public function __construct(){
+        $this->middleware('auth', ['except' => ['welcome']]);
+    }
+
     /**
      * Display a listing of the resource.
      *
